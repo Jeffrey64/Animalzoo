645ae3ac51413779ec2bd980f312e43c70cbef58
Jeffrey64 <143215928+Jeffrey64@users.noreply.github.com>
2024-01-05 00:50:47 -0800

    Add files via upload

---
+++ Zoo/LagunasJeffrey.java
@@ -0,0 +1,17 @@
+
+/**
+ * A Penguin is Jeffrey Lagunas's animal
+ * 
+ * @author Mr. Jaffe
+ * @version 1.0 2017-07-14
+ */
+public class LagunasJeffrey extends Animal
+{
+  public RogerJaffe() {
+    super("Roger Jaffe", "red", 0);
+  }
+  
+  public String getType() {
+    return "BoopleSnoot";
+  }
+}
