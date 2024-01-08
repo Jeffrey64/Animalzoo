8581af81b35b89dbc57920d634183d23af12ab1c
Jeffrey64 <143215928+Jeffrey64@users.noreply.github.com>
2024-01-08 01:01:21 -0800

    Add files via upload

---
+++ java-zoo-master/JavaZoo.java
@@ -0,0 +1,38 @@
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Write a description of class JavaZoo here.
+ * 
+ * @author Mr. Jaffe
+ * @version 2017-07-014
+ */
+public class JavaZoo extends Animal {
+  
+  @Override
+  public String getName() {
+    return "Gary";
+  }
+
+  public static void main(String[] args) {
+
+    final int DELAY = 4;
+        
+    // Change RogerJaffe to the name of your animal class and run the 'main' 
+    // method to test your code.  
+    String[] animalList = {"LagunasJeffrey"};
+    
+    try {
+      for (int i=0; i<animalList.length; i++) {
+        Object a = Class.forName(animalList[i]).newInstance();
+        System.out.println("Animal #"+(i+1)+" by "+((Animal)a).getStudentName());
+        System.out.println(a.toString());
+        System.out.println();
+        TimeUnit.SECONDS.sleep(DELAY);
+      }
+    } catch (Exception e) {
+      System.out.println("Someone messed up!");
+      System.out.println(e.toString());
+    }
+  }
+}
+
---
+++ java-zoo-master/LagunasJeffrey.java
@@ -0,0 +1,17 @@
+
+/**
+ * A Penguin is Jeffrey Lagunas's animal
+ * 
+ * @author Mr. Jaffe
+ * @version 1.0 2017-07-14
+ */
+public class LagunasJeffrey extends Animal
+{
+  public LagunasJeffrey() {
+    super("Lagunas Jeffrey", "black and white", 2);
+  }
+  
+  public String getType() {
+    return "Penguin";
+  }
+}
